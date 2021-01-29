04f4136ca2c13c21e606c1439290756200a281d9
Katelynn Lauricello <katelynn.lauricello@patelhs.org>
2021-01-29 16:40:38 +0000

    I added the scanner asking the user whether or not that are a mermaid.

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+import java.util.Scanner;
+  class Main 
+{
+  public static void main(String[] args) 
+  { 
+
+    Scanner scan = new scanner(System.in);
+    System.out.println("Are you a mermaid T/F ?");
+    String mermaidMaid = scan.nextBoolean();
+
+
+  }
+
+  static  mermaid(boolean mermaidMaid)
+  {
+    
+     return mermaid;
   }
 }
