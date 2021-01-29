2a7d1fb38578610f6b066179e55e0dfe12e31ced
Katelynn Lauricello <katelynn.lauricello@patelhs.org>
2021-01-29 17:21:40 +0000

    I finished the 1st method

--- Main.java
+++ Main.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
-  class Main 
-{
-  public static void main(String[] args) 
-  { 
+
+class Main {
+  public static void main(String[] args) {
 
     Scanner scan = new scanner(System.in);
     System.out.println("Are you a mermaid T/F ?");
-    String mermaidMaid = scan.nextBoolean();
+    boolean mermaidMaid = scan.nextBoolean();
 
-
+    System.out.println(mermaid(mermaidMaid));
   }
 
-  static  mermaid(boolean mermaidMaid)
-  {
-    
-     return mermaid;
+  static String mermaid(boolean mermaidMaid) {
+    if (mermaid) {
+
+      String mermaid = mermaidMaid + "So you are a mermaid. Knew it ";
+
+    } else {
+      String mermaid = mermaidMaid + "So you aren't a mermaid :( . I just lot a 500 dollar bet!";
+    }
+    return mermaid;
+
   }
 }
